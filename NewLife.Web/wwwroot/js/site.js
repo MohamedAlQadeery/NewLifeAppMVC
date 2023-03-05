e7effd9c2c8f4fd148d0c8379670214fda87c484
MohamedAlQadeery <moh9amad1@gmail.com>
2023-03-05 22:58:24 +0200

    displaying logged in admin info in header/added account settings page / signout button

--- NewLife.Web/wwwroot/js/site.js
+++ NewLife.Web/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const logoutBtn = document.querySelector('#logout-btn');
+
+logoutBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector('#logout-form').submit();
+
+})
+
