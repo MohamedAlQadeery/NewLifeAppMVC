36f796b8805917f2145fa7905cf0721527c2ac4b
MohamedAlQadeery <moh9amad1@gmail.com>
2023-03-05 16:31:32 +0200

    Prepare admin seed data

---
+++ NewLife.Web/Seed/DefaultUsers.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NewLife.Web.Core.Consts;
+
+namespace NewLife.Web.Seed
+{
+    public static class DefaultUsers
+    {
+        public static async Task SeedAdmin(UserManager<IdentityUser> userManager)
+        {
+            var admin = new IdentityUser {
+                UserName = "admin",
+                Email = "admin@newlife.com",
+                EmailConfirmed = true
+
+            };
+
+            var adminExist = await userManager.FindByNameAsync(admin.UserName);
+            if(adminExist is null)
+            {
+                await userManager.CreateAsync(admin,"Admin123@");
+                await userManager.AddToRoleAsync(admin, AppRoles.Admin);
+            }
+        }
+    }
+}
